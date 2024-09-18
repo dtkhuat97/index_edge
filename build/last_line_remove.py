8c25b5bf3494e726afdf646d16793154cb7566fb
dtkhuat97 <tkhuat97@gmail.com>
2024-09-18 14:40:52 +0200

    remove python and ipynb files

--- build/last_line_remove.py
+++
@@ -1,6 +0,0 @@
-import csv
-f = open('/home/dtkhuat/results/times/11_06_2024.csv', "r+")
-lines = f.readlines()
-lines.pop()
-f = open('/home/dtkhuat/results/times/11_06_2024.csv', "w+")
-f.writelines(lines)
--- build/pop_any_line.py
+++
@@ -1,6 +0,0 @@
-import csv
-with open('/home/dtkhuat/results/times/26_04_24.csv', 'r+') as inp, open('/home/dtkhuat/results/times/27_04_24.csv', 'w+') as out:
-    writer = csv.writer(out)
-    for row in csv.reader(inp):
-        if row[1] != "":
-            writer.writerow(row)
